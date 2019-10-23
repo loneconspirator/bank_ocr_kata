3b276f26fc609650c81d1904ff0da212a3c5b493
Mike McCracken <mike+github@technomonkey.net>
2019-10-23 09:28:35 -0700

    No longer using ocr_digit_line

--- lib/ocr_stream_reader.rb
+++ lib/ocr_stream_reader.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'ocr_digit_line'
-
 # Takes a filename as an input argument to build list of results
 class OcrStreamReader
   attr_reader :input_lines
