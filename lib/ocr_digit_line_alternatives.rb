24def298b8ea7b5086d45322529c214deba030be
Mike McCracken <mike+github@technomonkey.net>
2019-10-23 16:30:44 -0700

    Add output_line for alternative digit lines

--- lib/ocr_digit_line_alternatives.rb
+++ lib/ocr_digit_line_alternatives.rb
@@ -6,11 +6,14 @@ require_relative 'ocr_digit_alternatives'
 
 # Finds alternative valid digit lines for a given scan
 class OcrDigitLineAlternatives
-  attr_accessor :alternatives
+  attr_accessor :alternatives, :status, :output_line
 
   def initialize(digit_scans)
     @alternatives = (0...9).map { |pos| options_swapping(pos, digit_scans) }
                            .flatten.compact
+    @status = 'ILL' if @alternatives.empty?
+    @status = 'AMB' if @alternatives.length > 1
+    @output_line = build_output_line(digit_scans)
   end
 
   private
@@ -18,7 +21,7 @@ class OcrDigitLineAlternatives
   def options_swapping(position, digit_scans)
     original = digit_scans.map(&:digit_string)
     OcrDigitAlternatives.new(digit_scans[position].digit_scan)
-      .alternatives.map do |alt|
+                        .alternatives.map do |alt|
       original[position] = alt
       test_line = original.join('')
       line_if_good(test_line)
@@ -27,5 +30,24 @@ class OcrDigitLineAlternatives
 
   def line_if_good(line)
     line if OcrDigitLine.valid?(line) && !OcrDigitLine.illegible?(line)
+  end
+
+  def build_output_line(digit_scans)
+    case @alternatives.length
+    when 0
+      value_and_status(digit_scans)
+    when 1
+      @alternatives[0].to_s
+    else
+      "#{value_and_status(digit_scans)} #{format_alts(@alternatives)}"
+    end
+  end
+
+  def value_and_status(digit_scans)
+    "#{digit_scans.map(&:digit_string).join('')} #{status}"
+  end
+
+  def format_alts(alts)
+    '[' + alts.map { |val| "'#{val}'" }.join(', ') + ']'
   end
 end
--- test/ocr_digit_line_alternatives_test.rb
+++ test/ocr_digit_line_alternatives_test.rb
@@ -14,6 +14,7 @@ describe OcrDigitLineAlternatives do
     digit_line_obj = OcrDigitLine.new(ocr)
     alts_obj = OcrDigitLineAlternatives.new(digit_line_obj.ocr_digits)
     assert_equal ['711111111'].sort, alts_obj.alternatives.sort
+    assert_equal '711111111', alts_obj.output_line
   end
 
   it 'returns expected alternatives for 111111111' do
@@ -26,6 +27,8 @@ describe OcrDigitLineAlternatives do
     alts_obj = OcrDigitLineAlternatives.new(digit_line_obj.ocr_digits)
     assert_equal %w[888886888 888888880 888888988].sort,
                  alts_obj.alternatives.sort
+    assert_equal "888888888 AMB ['888886888', '888888988', '888888880']",
+                 alts_obj.output_line
   end
 
   it 'returns expected alternatives for 49086717?' do
@@ -37,5 +40,6 @@ describe OcrDigitLineAlternatives do
     digit_line_obj = OcrDigitLine.new(ocr)
     alts_obj = OcrDigitLineAlternatives.new(digit_line_obj.ocr_digits)
     assert_equal ['490867715'].sort, alts_obj.alternatives.sort
+    assert_equal '490867715', alts_obj.output_line
   end
 end
